3466dc26005b67f07ba4e63db3730850273e70e9
Ruel Dognidon <rueldognidon@gmail.com>
2020-09-03 05:59:06 +0000

    Co-authored-by: Marasigan Melquisedec John (ERNI) <mamj@erni.ph> Co-authored-by: Gacayan Edgar Allan (ERNI) <gaed@erni.ph> Co-authored-by: Dangcal Amoran (ERNI) <daam@erni.ph>

--- CodeWars/Class1.cs
+++ CodeWars/Class1.cs
@@ -4,5 +4,20 @@ namespace CodeWars
 {
     public class Class1
     {
+       public void Foo() 
+       {
+           Console.WriteLine("Hello World! I'm now a programmer"); 
+       }
+    }
+}
+
+
+namespace CodeWars
+{
+    public class Class2
+    {
+        public Class2()
+        {
+        }
     }
 }
